5abe3ba731ac0253079a83953dc7c0f5d5e3be1f
Ken Hasselmann <ken.hasselmann@mil.be>
2024-01-19 11:40:58 +0100

    added PKGBUILD and build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+set -euxo pipefail
+
+# Set the makedeb release you want.
+export MAKEDEB_RELEASE='makedeb'
+
+# Run the install script. Note that it's `bash -c` and not `bash -ci` now.
+bash -c "$(wget -qO - 'https://shlink.makedeb.org/install')"
+
+# build mongo-c
+cd mongo-c/
+makedeb -s PKGBUILD
+
+# build mongo-cxx
+cd mongo-cxx/
+makedeb -s PKGBUILD
+
